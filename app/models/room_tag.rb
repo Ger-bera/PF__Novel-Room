2882c894aee51bde5420e21637eec92fe9073556
Ger-bera <1925dart@gmail.com>
2021-10-20 09:03:03 +0000

    [Add]foreign_key: 'room_id'

--- app/models/room_tag.rb
+++ app/models/room_tag.rb
@@ -1,4 +1,4 @@
 class RoomTag < ApplicationRecord
-  has_many :room_tagmaps, dependent: :destroy
+  has_many :room_tagmaps, dependent: :destroy, foreign_key: 'room_id'
   has_many :rooms, through: :room_tagmaps
 end
