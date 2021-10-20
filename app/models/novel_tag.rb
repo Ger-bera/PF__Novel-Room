8c06beae4eda2e2cae4c3e8c4553b45d387d2c18
Ger-bera <1925dart@gmail.com>
2021-10-20 09:11:16 +0000

    [Add]foreign_key: 'novel_id'

--- app/models/novel_tag.rb
+++ app/models/novel_tag.rb
@@ -1,4 +1,4 @@
 class NovelTag < ApplicationRecord
-  has_many :novel_tagmaps, dependent: :destroy
+  has_many :novel_tagmaps, dependent: :destroy,foreign_key: 'novel_id'
   has_many :novels, through: :novel_tagmaps
 end
